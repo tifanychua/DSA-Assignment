e40f70f4dca36cd7cbee5bc002120b199126c91a
Tifany <tifanyclf-pm23@student.tarc.edu.my>
2025-07-19 13:15:54 +0800

    Adjust the GUI

--- MedicalTreatmentModule/src/recordDiagnosis.java
+++ MedicalTreatmentModule/src/recordDiagnosis.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;^
+import javax.swing.*;
+import java.time.LocalDate;
+
 
 // UI(view) -> control -> DA(model) -> Domain
 
@@ -20,60 +22,82 @@ public class recordDiagnosis extends JFrame {
     private JLabel topLabel=new JLabel("Patient Diagnosis Record");
 
     public recordDiagnosis() {
-        JPanel jpTop=new JPanel(new FlowLayout());
-        JPanel jpInfo=new JPanel(new FlowLayout(FlowLayout.CENTER,20,15));
-        Font font = new Font("SansSerif", Font.BOLD, 48); 
+        JPanel jpTop=new JPanel();
+        JPanel jpInfo=new JPanel(new FlowLayout(FlowLayout.LEFT,100,5));
+        Font font = new Font("SansSerif", Font.BOLD, 56); 
         topLabel.setFont(font);
-        jpTop.add(topLabel);
-        JPanel jpCenter = new JPanel(new GridLayout(6, 2));
-        JLabel labelTreatmentID = new JLabel("Treatment ID:");
-        labelTreatmentID.setFont(new Font("SansSerif", Font.BOLD, 18));
+        jpTop.add(topLabel,BorderLayout.CENTER);
+        JPanel jpCenter = new JPanel(new GridLayout(7, 2,50,20));
+        JLabel labelTreatmentID = new JLabel("Treatment ID: "+"2507191001");
+                labelTreatmentID.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 1000));
+
+        labelTreatmentID.setFont(new Font("SansSerif", Font.PLAIN, 32));
         jpInfo.add(labelTreatmentID);
-        jpInfo.add(jtfTreatID);
-        JLabel labelPatient = new JLabel("Patient Name:");
-        labelPatient.setFont(new Font("SansSerif", Font.BOLD, 18));
-        jpInfo.add(labelPatient);
-        jpInfo.add(jtfPatientName);
-        JLabel labelDate = new JLabel("Date");
-        labelDate.setFont(new Font("SansSerif", Font.BOLD, 18));
+        LocalDate today = LocalDate.now();
+        JLabel labelDate = new JLabel("Date: "+today.toString());
+        labelDate.setFont(new Font("SansSerif", Font.PLAIN, 32));
         jpInfo.add(labelDate);
-        jpInfo.add(jtfDate);
+        JLabel labelPatient = new JLabel("Patient Name");
+        labelPatient.setFont(new Font("SansSerif", Font.BOLD, 40));
+        jpCenter.add(labelPatient);
+        jpCenter.add(jtfPatientName);
         JLabel labelDoctor = new JLabel("Doctor");
-        labelDoctor.setFont(new Font("SansSerif", Font.BOLD, 18));
+        labelDoctor.setFont(new Font("SansSerif", Font.BOLD, 40));
         jpCenter.add(labelDoctor);
         jpCenter.add(jtfDoctor);
         JLabel labelDiagnosis = new JLabel("Diagnosis");
-        labelDiagnosis.setFont(new Font("SansSerif", Font.BOLD, 18));
+        labelDiagnosis.setFont(new Font("SansSerif", Font.BOLD, 40));
         jpCenter.add(labelDiagnosis);
         jpCenter.add(jtfDiagnosis);
         JLabel labelType = new JLabel("Type of Treatment");
-        labelType.setFont(new Font("SansSerif", Font.BOLD, 18));
+        labelType.setFont(new Font("SansSerif", Font.BOLD, 40));
         jpCenter.add(labelType);
         jpCenter.add(jtfTreatment);
         JLabel labelTreatment = new JLabel("Treatment Notes");
-        labelTreatment.setFont(new Font("SansSerif", Font.BOLD, 18));
+        labelTreatment.setFont(new Font("SansSerif", Font.BOLD, 40));
         jpCenter.add(labelTreatment);
         jpCenter.add(jtfNote);
         JLabel labelPatientStatus = new JLabel("Patient Status");
-        labelPatientStatus.setFont(new Font("SansSerif", Font.BOLD, 18));
+        labelPatientStatus.setFont(new Font("SansSerif", Font.BOLD, 40));
         jpCenter.add(labelPatientStatus);
         jpCenter.add(jtfPatient);
         JLabel labelFollowUp = new JLabel("Follow-up Required");
-        labelFollowUp.setFont(new Font("SansSerif", Font.BOLD, 18));
+        labelFollowUp.setFont(new Font("SansSerif", Font.BOLD, 40));
         jpCenter.add(labelFollowUp);
-        jpCenter.add(jtfFollowUp);
+        ButtonGroup followButton=new ButtonGroup();
+        JPanel buttonPanel=new JPanel(new FlowLayout(FlowLayout.LEFT, 30, 0));
+        JRadioButton yesBtn=new JRadioButton("Yes");
+        JRadioButton noBtn=new JRadioButton("No");
+    followButton.add(yesBtn);
+    followButton.add(noBtn);
+    buttonPanel.add(yesBtn);
+    buttonPanel.add(noBtn);
+    
+    Font rbFont = new Font("SansSerif", Font.BOLD, 35);
+    yesBtn.setFont(rbFont);
+    noBtn.setFont(rbFont);
+    jpCenter.add(buttonPanel,BorderLayout.WEST);
         add(jpTop,BorderLayout.NORTH);
         add(jpInfo);
-        JPanel jpSouth = new JPanel();
+        JPanel jpSouth = new JPanel(new FlowLayout(FlowLayout.CENTER, 40, 20));
         JPanel jpForm=new JPanel(new BorderLayout());
-        jpForm.add(jpInfo,BorderLayout.NORTH);
+        JPanel infoForm=new JPanel(new FlowLayout(FlowLayout.LEFT));
+        infoForm.add(jpInfo,FlowLayout.LEFT);
+        add(infoForm,BorderLayout.NORTH);
+        jpForm.add(jpTop,BorderLayout.NORTH);
         jpForm.add(jpCenter);
-        add(jpForm);
+        JPanel formPn=new JPanel(new FlowLayout());
+        formPn.add(jpForm);
+        add(formPn);
         jpSouth.add(jbtAdd);
         jpSouth.add(jbtDelete);
         add(jpSouth, BorderLayout.SOUTH);
-        
-        
+        jpInfo.setBorder(BorderFactory.createEmptyBorder(30, 0, 20, 0));
+        jpTop.setBorder(BorderFactory.createEmptyBorder(0, 0, 90, 0));
+        jpSouth.setBorder(BorderFactory.createEmptyBorder(0, 0, 30, 0));
+        Dimension buttonSize = new Dimension(150, 50); 
+jbtAdd.setPreferredSize(buttonSize);
+jbtDelete.setPreferredSize(buttonSize);
     }
     
     
